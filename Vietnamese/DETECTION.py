f895a6d2c9cacc108280cb65fa2b03a0191b429e
poohoh <kjh980328@gmail.com>
2023-09-15 14:21:48 +0900

    update detect all images in directory

--- Vietnamese/DETECTION.py
+++ Vietnamese/DETECTION.py
@@ -10,6 +10,7 @@ from typing import List
 # from dynaconf import settings
 from models.experimental import attempt_load
 import cv2
+from tqdm import tqdm
 
 class Detection:
     def __init__(self, weights_path='.pt',size=(640,640),device='cuda',iou_thres=None,conf_thres=None):
@@ -59,7 +60,7 @@ class Detection:
                 det=det.tolist()
                 if len(det):
                     for *xyxy, conf, cls in det:
-                        if "car" in self.names[int(cls)]:
+                        if "car" in self.names[int(cls)] or "truck" in self.names[int(cls)] or "bus" in self.names[int(cls)]:
                             # xc,yc,w_,h_=(xyxy[0]+xyxy[2])/2,(xyxy[1]+xyxy[3])/2,(xyxy[2]-xyxy[0]),(xyxy[3]-xyxy[1])
                             result=[self.names[int(cls)], str(conf), (xyxy[0],xyxy[1],xyxy[2],xyxy[3])]
                             results.append(result)
@@ -116,7 +117,7 @@ def parse_opt():
     parser.add_argument('--weights', nargs='+', type=str, default='object.pt', help='model path or triton URL')
     parser.add_argument('--source', type=str, default='input', help='file/dir')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[1280], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.1, help='confidence threshold')
+    parser.add_argument('--conf-thres', type=float, default=0.5, help='confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.5, help='NMS IoU threshold')
     parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
     parser.add_argument('--device', default='cuda', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
@@ -128,14 +129,15 @@ def parse_opt():
 
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+def main():
     opt = parse_opt()
     
     char_model=Detection(size=opt.imgsz,weights_path=opt.weights,device=opt.device,iou_thres=opt.iou_thres,conf_thres=opt.conf_thres)
 
     # path=opt.source
     path = os.path.abspath("C:\\annotation\\dataset\\230719_camera(9)_7-8\\output1")  # 이 부분 수정
-    out_path = os.path.join(path, '..\\output1_cropped')  # 이 부분 수정
+    out_path = os.path.join(path, '..\\output1_detected')  # 이 부분 수정
 
     img_names=os.listdir(path)
 
@@ -153,30 +155,76 @@ if __name__ == '__main__':
         if results:
             os.makedirs(filepath, exist_ok=True)
 
-        i = 0
-        for name,conf,box in results:
-            y1, y2, x1, x2 = int(box[1]) - 5, int(box[3]) + 5, int(box[0]) - 5, int(box[2]) + 5
-            height = img.shape[0]
-            width = img.shape[1]
-            y1, y2, x1, x2 = max(0, y1), min(height, y2), max(0, x1), min(width, x2)
-            crop_img = resized_img[y1:y2, x1:x2]
+            cv2.imwrite(out_path, img)
 
-            #save_path = os.path.join(filepath, img_name[:-4]+'_'+ str(i) + '.png')
-            save_path = os.path.join(out_path, img_name[:-4]+'_'+ str(i) + '.png')  # 이 부분 수정
-
-            if crop_img.size:
-                cv2.imwrite(save_path, crop_img)
-
-            i += 1
-            '''
-            resized_img=cv2.putText(resized_img, "{}".format(name), (int(box[0]), int(box[1])-3),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                                    (255, 0, 255), 2)
-            resized_img = cv2.rectangle(resized_img, (int(box[0]),int(box[1])), (int(box[2]),int(box[3])), (0,0,255), 1)
-            '''
+        #### detect한 부분만 crop할 때 사용 ####
+        # i = 0
+        # for name,conf,box in results:
+        #     y1, y2, x1, x2 = int(box[1]) - 5, int(box[3]) + 5, int(box[0]) - 5, int(box[2]) + 5
+        #     height = img.shape[0]
+        #     width = img.shape[1]
+        #     y1, y2, x1, x2 = max(0, y1), min(height, y2), max(0, x1), min(width, x2)
+        #     crop_img = resized_img[y1:y2, x1:x2]
+        #
+        #     #save_path = os.path.join(filepath, img_name[:-4]+'_'+ str(i) + '.png')
+        #     save_path = os.path.join(out_path, img_name[:-4]+'_'+ str(i) + '.png')  # 이 부분 수정
+        #
+        #     if crop_img.size:
+        #         cv2.imwrite(save_path, crop_img)
+        #
+        #     i += 1
+        #     '''
+        #     resized_img=cv2.putText(resized_img, "{}".format(name), (int(box[0]), int(box[1])-3),
+        #                             cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+        #                             (255, 0, 255), 2)
+        #     resized_img = cv2.rectangle(resized_img, (int(box[0]),int(box[1])), (int(box[2]),int(box[3])), (0,0,255), 1)
+        #     '''
 
         # print('image saved:', filepath)
-        print('completed:', os.path.splitext(img_name)[0])
+        print('image saved:', os.path.splitext(img_name)[0])
 
         # cv2.imwrite(os.path.join('out',img_name),resized_img)
 
+def retrieve_images(root_path):
+    opt = parse_opt()
+
+    char_model = Detection(size=opt.imgsz, weights_path=opt.weights, device=opt.device, iou_thres=opt.iou_thres,
+                           conf_thres=opt.conf_thres)
+
+    read_files(root_path, char_model)
+
+def read_files(path, char_model):
+    detect_car(path, char_model)
+
+    directories = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+
+    if directories:
+        for directory in directories:
+            read_files(os.path.join(path, directory), char_model)
+
+def detect_car(path, char_model):
+    images = sorted(list(f for f in os.listdir(path) if f.endswith('.png') or f.endswith('.jpg')))
+
+    print(f'processing: {path}')
+
+    out_path = os.path.join(path, '..', 'detect', f'{os.path.split(path)[-1]}_detect')
+    os.makedirs(out_path, exist_ok=True)
+
+    i = 0
+    for image in tqdm(images):
+        img = cv2.imread(os.path.join(path, image))
+
+        results, resized_img=char_model.detect(img.copy())
+
+        # print(f'processing: {path + image}')
+
+        if results:
+            cv2.imwrite(os.path.join(out_path, f'{i:06d}.png'), img)
+            # print(f'image saved: {image}')
+
+        i += 1
+    # print('finished')
+
+
+root_path = 'C:/Users/KimJunha/Desktop/vision nerf vietnam/input/D3/5'
+retrieve_images(root_path)
